02f2c83c14cdf346f2369e80c8dcb28520c925f2
levankhoi19872004-pixel <levankhoi19872004@gmail.com>
2026-06-05 20:10:50 +0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const { startServer } = require('./src/app');
-
-startServer().catch((err) => {
-  console.error('Không thể khởi động server:', err);
-  process.exit(1);
-});
